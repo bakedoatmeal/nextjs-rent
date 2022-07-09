73c354c2a11a38481e951aac28d64888d5e9da99
Jane <jane.anqi.cui@gmail.com>
2022-07-09 18:42:54 -0400

    seed database

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.rentLog.create({
+    data: {
+      name: `Jane`,
+      email: `jane@hotmail.com`, 
+      rentalType: 'APARTMENT',
+      rentalSize: 'THREEANDAHALF',
+      address: '4057 Avenue Van Horne',
+      year: 2020,
+      rent: 2000,
+    },
+  })
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
