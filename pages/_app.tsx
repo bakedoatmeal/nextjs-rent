7651fc82a6d79e4754fd8b38a7ded4bc3b01c2d6
Jane <jane.anqi.cui@gmail.com>
2022-07-09 20:41:21 -0400

    update rents page to show results from database

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import 'styles/tailwind.css';
+import {ApolloProvider} from '@apollo/client'
+import apolloClient from 'lib/apollo';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-gray-900 py-56">
-      <Component {...pageProps} />
-    </div>
+    <ApolloProvider client={apolloClient}>
+      <div className="bg-gray-900 py-56">
+        <Component {...pageProps} />
+      </div>
+    </ApolloProvider>
   );
 }
 
